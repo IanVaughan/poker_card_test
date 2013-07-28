811d727e7e0bbd565a514aa992db3055f0011570
Ian Vaughan <github@ianvaughan.co.uk>
2013-07-28 16:14:50 +0100

    tidy spec

--- spec/pack_spec.rb
+++ spec/pack_spec.rb
@@ -2,45 +2,54 @@ require 'rspec/core'
 require './lib/pack'
 
 describe Pack do
-  subject { described_class.new }
+  let(:pack) { described_class.new }
+  subject(:dealt) { pack.cards }
 
   context "setup" do
     it "starts of with 52 cards" do
-      subject.count.should == 52
+      dealt.count.should == 52
     end
 
     it "has an array of cards" do
-      subject.should be_an_instance_of(Array)
+      dealt.should be_an_instance_of(Array)
     end
 
     it "has unique cards" do
-      subject.uniq!.should be_nil
+      dealt.uniq!.should be_nil
     end
 
     it "has cards as I expect" do
-      subject[0].should == '2C'
-      subject[1].should == '3C'
+      dealt[0].should == '2C'
+      dealt[1].should == '3C'
     end
   end
 
   context "dealing cards" do
     # A poker hand consists of 5 cards dealt from the deck.
     # so, just dealing with 5 card deal atm
-    let(:dealt) { subject.deal(5) }
+    let(:hand) { pack.deal(5) }
 
     it "gives a random card from the pack" do
-      dealt.should be_an_instance_of(Array)
-      dealt.count.should == 5
+      hand.should be_an_instance_of(Array)
+      hand.count.should == 5
     end
 
     it "has 5 less cards in the pack" do
-      subject.count.should == 52 - 5
+      dealt.count.should == 52 - 5
+    end
+
+    it "should have unique cards in the dealt hand" do
+      hand.uniq!.should be_nil
+    end
+
+    it "should not have the dealt cards in the pack" do
+      (dealt + hand).uniq!.should be_nil
     end
 
     it "cannot deal more cards than are in the pack" do
       # Could raise or return how many are left, depends whats best later on really.
-      subject.deal(55).should be_nil
-      subject.count.should == 52 - 5
+      pack.deal(55).should be_nil
+      dealt.count.should == 52 - 5
     end
   end
 end
