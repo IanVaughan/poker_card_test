e4a5c5d4cf62307ad5a084455d4ad0590df26d76
Ian Vaughan <github@ianvaughan.co.uk>
2013-07-28 17:46:57 +0100

    added sort and sequential?

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,4 +1,5 @@
 require 'card'
+require 'pack'
 
 class Hand
   def initialize cards
@@ -7,5 +8,17 @@ class Hand
 
   def same_suit?
     @cards.all? { |s| s.suit == @cards.first.suit }
+  end
+
+  def sort
+    @cards.sort { |c1, c2| Pack::VALUES.index(c1.value) <=> Pack::VALUES.index(c2.value) }
+  end
+
+  def sequential?
+    ordered = sort
+    start_index = Pack::VALUES.index(ordered.first.value)
+    ordered.each_cons(2).all? do |c1, c2|
+      Pack::VALUES.index(c1.value) == Pack::VALUES.index(c2.value) - 1
+    end
   end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -26,4 +26,43 @@ describe Hand do
       end
     end
   end
+
+  describe '#sort' do
+  end
+
+  describe '#sequential?' do
+    context 'when the cards are sequential in number' do
+      context 'simple case' do
+        let(:cards) { [Card.new('3', 'H'), Card.new('2', 'C')] }
+
+        it 'returns true' do
+          subject.sequential?.should be_true
+        end
+      end
+
+      context 'high value case' do
+        let(:cards) { [Card.new('Q', 'H'), Card.new('J', 'C')] }
+
+        it 'returns true' do
+          subject.sequential?.should be_true
+        end
+      end
+
+      context 'many values case' do
+        let(:cards) { [Card.new('Q', 'H'), Card.new('J', 'C'), Card.new('K', 'C')] }
+
+        it 'returns true' do
+          subject.sequential?.should be_true
+        end
+      end
+    end
+
+    context 'when the cards are not sequential in number' do
+      let(:cards) { [Card.new('2', 'H'), Card.new('4', 'C')] }
+
+      it 'returns false' do
+        subject.sequential?.should be_false
+      end
+    end
+  end
 end
